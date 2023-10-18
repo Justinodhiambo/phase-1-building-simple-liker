57736082d90bdb13d09f00624748233f9e245586
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2023-10-18 08:55:51 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+document.addEventListener("DOMContentLoaded", () => {
+  const heartIcon = document.querySelector(".like-glyph");
+  const errorModal = document.querySelector("#modal");
 
-// Your JavaScript code goes here!
+  errorModal.classList.add("hidden");
 
+  function handleSuccessResponse() {
+    heartIcon.classList.add("activated-heart");
+  }
 
+  function handleErrorResponse(errorMsg) {
+    errorModal.classList.remove("hidden");
+    errorModal.textContent = errorMsg;
 
+    setTimeout(() => {
+      errorModal.classList.add("hidden");
+    }, 3000);
+  }
 
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
+  heartIcon.addEventListener("click", () => {
+    mimicServerCall()
+      .then((response) => {
+        // Check if the server response is a success (no need to inspect response data)
+        if (response === "success") {
+          handleSuccessResponse();
+        } else {
+          // Handle the error response
+          handleErrorResponse("Server request failed.");
+        }
+      })
+      .catch((error) => {
+        // Handle any potential network or other errors
+        handleErrorResponse("Server request failed.");
+      });
   });
-}
+
+  heartIcon.addEventListener("click", () => {
+    // Toggle the heart icon on click
+    if (heartIcon.classList.contains("activated-heart")) {
+      heartIcon.classList.remove("activated-heart");
+    } else {
+      // Add the class to make the heart appear red
+      heartIcon.classList.add("activated-heart");
+    }
+  });
+});
